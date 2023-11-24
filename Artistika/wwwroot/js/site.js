dd4ee54d13a89a02404cb3d0a13af5416b173cdc
fkabigting <leonardfrancis.kabigting1@my-aolcc.com>
2023-11-23 21:07:37 -0700

    Add project files.

---
+++ Artistika/wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+document.querySelector('.animation-box').addEventListener('click', function () {
+    this.style.animation = 'bounce 1s ease-in-out';
+    // After the animation, remove the animation property to reset it for future clicks
+    this.addEventListener('animationend', function () {
+        this.style.animation = '';
+    });
+});
